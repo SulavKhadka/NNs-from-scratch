039ed35ef78f1962b1ffd572a4e4eecd548da568
Sulav Khadka <khadkasulav@gmail.com>
2024-07-19 14:05:35 -0700

    implemented weight init(kinda confusing), reimplemented multiHeadAttn(success), DataloaderLite and the training loop

--- spaced_rep_gpt2_model.py
+++ spaced_rep_gpt2_model.py
@@ -22,6 +22,7 @@ class CausalSelfAttention(nn.Module):
 
         self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd)
         self.c_proj = nn.Linear(config.n_embd, config.n_embd)
+        self.c_proj.NANOGPT_SCALE_INIT = 1
         self.register_buffer('bias', torch.tril(torch.ones(config.block_size, config.block_size)
                                                 .view(1, 1, config.block_size, config.block_size)))
         self.attn_dropout = nn.Dropout(config.dropout)
@@ -29,27 +30,28 @@ class CausalSelfAttention(nn.Module):
 
         self.n_embd = config.n_embd
         self.n_head = config.n_head
-    
+
     def forward(self, x):
         B, T, C = x.shape
 
-        q, k, v = self.c_attn(x).split(self.n_embd, dim=2)
-        # same ops on q, k, v: (B, T, C) -> (B, T, n_head, head_size) -> (B, n_head, T, head_size)
-        q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
+        exploded_attn: Tensor = self.c_attn(x)
+        q, k, v = exploded_attn.split(self.n_embd, dim=-1)
+        q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, T, C) -> (B, T, n_head, head_size) -> (B, n_head, T, head_size)
         k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
         v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
 
-        wei: Tensor = (q @ k.transpose(-2, -1)) * (1 / math.sqrt(q.size(-1))) 
-        wei = wei.masked_fill(self.bias[:,:,:T,:T] == 0, float('-inf'))
+        wei: Tensor = (q @ k.transpose(-2, -1)) / math.sqrt(k.size(-1)) # (B, n_head, T, head_size) @ (B, n_head, head_size, T) -> (B, n_head, T, T)
+        wei = wei.masked_fill(self.bias[:, :, :T, :T] == 0, float('-inf')) # cant forget to crop the bias mask to T(num_tokens) of the current input x
         wei = wei.softmax(dim=-1)
-        wei = self.attn_dropout(wei) # wei.shape = (B, n_head, T, T)
+        wei = self.attn_dropout(wei)
 
         out: Tensor = wei @ v # (B, n_head, T, T) @ (B, n_head, T, head_size) -> (B, n_head, T, head_size)
-        out = out.transpose(1, 2).contiguous().view(B, T, C)
+        out = out.transpose(1, 2).contiguous().view(B, T, C) # (B, n_head, T, head_size) -> (B, T, n_head, head_size) -> (B, T, C)
         out = self.c_proj(out)
         out = self.resid_dropout(out)
 
         return out
+
 
 class MLP(nn.Module):
     def __init__(self, config: GPTConfig):
@@ -58,6 +60,7 @@ class MLP(nn.Module):
         self.c_fc = nn.Linear(config.n_embd, 4 * config.n_embd)
         self.gelu = nn.GELU(approximate='tanh')
         self.c_proj = nn.Linear(4 * config.n_embd, config.n_embd)
+        self.c_proj.NANOGPT_SCALE_INIT = 1
         self.dropout = nn.Dropout(config.dropout)
     
     def forward(self, x):
@@ -86,7 +89,7 @@ class Block(nn.Module):
 class GPT(nn.Module):
     def __init__(self, config: GPTConfig):
         super().__init__()
-
+        self.config = config
         self.transformer = nn.ModuleDict(dict(
             wte = nn.Embedding(config.vocab_size, config.n_embd),
             wpe = nn.Embedding(config.block_size, config.n_embd),
@@ -95,8 +98,22 @@ class GPT(nn.Module):
         ))
         self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
         self.lm_head.weight = self.transformer.wte.weight # weight typing as followed in the original GPT-2 implementation. pop quiz: but why?
-        self.block_size = config.block_size
-    
+
+        self.apply(self._init_weights) # apply weight initializtion to every module with logic described in _init_weights()
+
+    def _init_weights(self, module: nn.Module):
+        # if linear layer apply std dev of 0.02 and mean of 0 | if bias then initialize those with 0
+        if isinstance(module, nn.Linear):
+            std = 0.02
+            if hasattr(module, 'NANOGPT_SCALE_INIT'):
+                std = std * (1 / math.sqrt(2*self.config.n_layer))
+            torch.nn.init.normal_(module.weight, mean=0, std=std)
+            if module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+        # if embedding layer apply std dev of 0.02 and mean of 0
+        elif isinstance(module, nn.Embedding):
+            torch.nn.init.normal_(module.weight, mean=0, std=0.02)
+
     def forward(self, x, targets=None):
         B, T = x.shape
 
@@ -176,7 +193,7 @@ class GPT(nn.Module):
         B, T = idx.shape
 
         for i in range(max_new_tokens):
-            idx_crop = idx[:, -self.block_size:]
+            idx_crop = idx[:, -self.config.block_size:]
             logits, loss = self(idx_crop)
 
             logits: Tensor = logits[:, -1, :]
@@ -189,6 +206,30 @@ class GPT(nn.Module):
             idx = torch.cat((idx, new_tok), dim=-1)
         return idx
 
+# ---------------------------------------------- Dataloader ----------------------------------------------
+
+class DataloaderLite():
+    def __init__(self, B, T):
+        self.B = B
+        self.T = T
+
+        with open("input.txt", "r") as file:
+            text = file.read()
+        enc = tiktoken.get_encoding('gpt2')
+        self.tokens = torch.tensor(enc.encode(text))
+        self.curr_pos = 0
+
+    def get_batch(self):
+        batch_tokens = self.tokens[self.curr_pos: self.curr_pos + (self.B * self.T) + 1]
+        x = batch_tokens[:-1].view(self.B, self.T)
+        y = batch_tokens[1:].view(self.B, self.T)
+
+        self.curr_pos += self.B * self.T
+        if self.curr_pos + self.B * self.T + 1 > len(self.tokens):
+            self.curr_pos = 0
+        
+        return x, y
+
 
 # ---------------------------------------------- Inference ----------------------------------------------
 device = 'cpu'
@@ -199,16 +240,35 @@ elif hasattr(torch.backends, 'mps') and torch.backends.mps.is_available():
 print(f"using device: {device}")
 
 
-model = GPT.from_pretrained('gpt2')
+# model = GPT.from_pretrained('gpt2')
+# model.to(device)
+
+# enc = tiktoken.get_encoding('gpt2')
+# encoded_text = enc.encode("I am a language model,")
+# batch = [encoded_text for _ in range(5)]
+# tokens = torch.tensor(batch, device=device)
+
+# x = model.generate(tokens, max_new_tokens=50)
+
+# gen_text = enc.decode_batch(x.tolist())
+# for i in gen_text:
+#     print(i)
+
+# ---------------------------------------------- Training Loop ----------------------------------------------
+
+model: GPT = GPT(GPTConfig)
 model.to(device)
 
-enc = tiktoken.get_encoding('gpt2')
-encoded_text = enc.encode("I am a language model,")
-batch = [encoded_text for _ in range(5)]
-tokens = torch.tensor(batch, device=device)
+num_steps = 50
+dataloader = DataloaderLite(B=4, T=1024)
+optim = torch.optim.AdamW(model.parameters(), lr=3e-4)
 
-x = model.generate(tokens, max_new_tokens=50)
+for step in range(num_steps):
+    x, y = dataloader.get_batch()
+    x, y = x.to(device), y.to(device)
 
-gen_text = enc.decode_batch(x.tolist())
-for i in gen_text:
-    print(i)
+    optim.zero_grad()
+    logits, loss = model(x, targets=y)
+    loss.backward()
+    optim.step()
+    print(f"step {step}: loss = {loss.item()}")
