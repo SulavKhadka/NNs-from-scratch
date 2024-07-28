7d2e900e662baaf52d7e962a95440d67df57a50d
Sulav Khadka <khadkasulav@gmail.com>
2024-07-28 16:17:14 -0700

    matched the hf model of bert enough to load weights and generate predictions. but still it outputs gibberish :/ idk

--- model_implementations/bert_from_scratch.py
+++ model_implementations/bert_from_scratch.py
@@ -4,15 +4,16 @@ from torch import Tensor
 import torch.nn as nn
 from torch.nn import functional as F
 from dataclasses import dataclass
-from transformers import BertTokenizer
+from transformers import AutoModelForMaskedLM, BertConfig, BertModel, BertTokenizer, load_tf_weights_in_bert
+
 
 @dataclass
 class skBERTConfig:
-    vocab_size: 30522
-    block_size: 512
-    n_embd: 768
-    n_layer: 12
-    n_head: 12
+    vocab_size: int = 30522
+    max_position_embeddings: int = 512
+    hidden_size: int = 768
+    num_hidden_layers: int = 12
+    num_attention_heads: int = 12
 
 
 class MultiHeadAttention(nn.Module):
@@ -20,32 +21,33 @@ class MultiHeadAttention(nn.Module):
         super().__init__()
 
         self.self = nn.ModuleDict(dict(
-            query = nn.Linear(config.n_embd, config.n_embd),
-            key = nn.Linear(config.n_embd, config.n_embd),
-            value = nn.Linear(config.n_embd, config.n_embd)
+            query = nn.Linear(config.hidden_size, config.hidden_size),
+            key = nn.Linear(config.hidden_size, config.hidden_size),
+            value = nn.Linear(config.hidden_size, config.hidden_size)
         ))
         self.output = nn.ModuleDict(dict(
-            dense = nn.Linear(config.n_embd, config.n_embd),
-            LayerNorm = nn.LayerNorm(config.n_embd)
+            dense = nn.Linear(config.hidden_size, config.hidden_size),
+            LayerNorm = nn.LayerNorm(config.hidden_size)
         ))
-        self.n_head = config.n_head
+        self.num_attention_heads = config.num_attention_heads
 
     def forward(self, x, attn_mask: Tensor = None):
         B, T, C = x.shape
 
-        # (B, T, C) -> (B, T, n_head, head_size) -> (B, n_head, T, head_size)
-        q = self.self.query(x).view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
-        k = self.self.key(x).view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
-        v = self.self.value(x).view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
+        # (B, T, C) -> (B, T, num_attention_heads, head_size) -> (B, num_attention_heads, T, head_size)
+        q = self.self.query(x).view(B, T, self.num_attention_heads, C // self.num_attention_heads).transpose(1, 2)
+        k = self.self.key(x).view(B, T, self.num_attention_heads, C // self.num_attention_heads).transpose(1, 2)
+        v = self.self.value(x).view(B, T, self.num_attention_heads, C // self.num_attention_heads).transpose(1, 2)
 
-        # (B, n_head, T, head_size) @ (B, n_head, head_size, T) -> (B, n_head, T, T)
+        # (B, num_attention_heads, T, head_size) @ (B, num_attention_heads, head_size, T) -> (B, num_attention_heads, T, T)
         wei: Tensor = (q @ k.transpose(-2, -1)) * (1 / math.sqrt(q.size(-1)))
         if attn_mask is not None:
+            attn_mask = attn_mask[:, None, None, :]
             wei = wei.masked_fill(attn_mask == 0, float('-inf'))
         wei = wei.softmax(-1)
-        out = wei @ v # (B, n_head, T, T) @ (B, n_head, T, head_size) -> (B, n_head, T, head_size)
+        out = wei @ v # (B, num_attention_heads, T, T) @ (B, num_attention_heads, T, head_size) -> (B, num_attention_heads, T, head_size)
 
-        out = out.transpose(1, 2).contiguous().view(B, T, C) # (B, n_head, T, head_size) -> (B, T, n_head, head_size) -> (B, T, C == n_head*head_size)
+        out = out.transpose(1, 2).contiguous().view(B, T, C) # (B, num_attention_heads, T, head_size) -> (B, T, num_attention_heads, head_size) -> (B, T, C == n_head*head_size)
         out = self.output.dense(out)
         out = self.output.LayerNorm(out)
 
@@ -56,17 +58,19 @@ class Block(nn.Module):
         super().__init__()
 
         self.attention = MultiHeadAttention(config)
-        self.intermediate = nn.Linear(config.n_embd, 4 * config.n_embd)
+        self.intermediate = nn.ModuleDict(dict(
+            dense = nn.Linear(config.hidden_size, 4 * config.hidden_size)
+        ))
         self.output = nn.ModuleDict(dict(
-            dense = nn.Linear(4 * config.n_embd, config.n_embd),
-            LayerNorm = nn.LayerNorm(config.n_embd)
+            dense = nn.Linear(4 * config.hidden_size, config.hidden_size),
+            LayerNorm = nn.LayerNorm(config.hidden_size)
         ))
 
     def forward(self, x, attn_mask = None):
         x = x + self.attention(x, attn_mask)
         
         # Perform MLP
-        mlp_x = self.intermediate(x)
+        mlp_x = self.intermediate.dense(x)
         mlp_x = F.gelu(mlp_x)
         mlp_x = self.output.dense(mlp_x)
         mlp_x = self.output.LayerNorm(mlp_x)
@@ -77,29 +81,29 @@ class Block(nn.Module):
 
 class skBERT(nn.Module):
     def __init__(self, config: skBERTConfig):
-        super().__init()
+        super().__init__()
 
         self.embeddings = nn.ModuleDict(dict(
-            word_embeddings = nn.Embedding(config.vocab_size, config.n_embd),
-            position_embeddings = nn.Embedding(config.block_size, config.n_embd),
-            token_type_embeddings = nn.Embedding(2, config.n_embd),
-            LayerNorm = nn.LayerNorm(config.n_embd)
+            word_embeddings = nn.Embedding(config.vocab_size, config.hidden_size),
+            position_embeddings = nn.Embedding(config.max_position_embeddings, config.hidden_size),
+            token_type_embeddings = nn.Embedding(2, config.hidden_size),
+            LayerNorm = nn.LayerNorm(config.hidden_size)
         ))
         self.encoder = nn.ModuleDict(dict(
-            layer = nn.ModuleList([Block(config) for _ in config.n_layer])
+            layer = nn.ModuleList([Block(config) for _ in range(config.num_hidden_layers)])
         ))
     
     def forward(self, x, attn_mask=None, token_type_ids=None):
         B, T = x.shape
 
         tok_embed = self.embeddings.word_embeddings(x)
-        pos_embed = self.embeddings.position_embeddings(torch.arange(T, dtype=torch.long))
+        pos_embed = self.embeddings.position_embeddings(torch.arange(T, dtype=torch.long, device=x.device))
         if token_type_ids is None:
             token_type_ids = torch.zeros_like(x)
         token_type_embed = self.embeddings.token_type_embeddings(token_type_ids)
         
-        inputs = tok_embed + pos_embed + token_type_embed
-        inputs = self.embeddings.LayerNorm(inputs)
+        x = tok_embed + pos_embed + token_type_embed
+        x = self.embeddings.LayerNorm(x)
 
         for block in self.encoder.layer:
             x = block(x, attn_mask)
@@ -107,34 +111,47 @@ class skBERT(nn.Module):
         return x # (B, T, C)
 
 
-class skBERTForMaskedLM(nn.Module):
+class BERTPredictionHeadLM(nn.Module):
     def __init__(self, config: skBERTConfig):
         super().__init__()
 
+        self.bias = nn.Parameter(torch.zeros(config.vocab_size))
+        self.transform = nn.ModuleDict(dict(
+            dense = nn.Linear(config.hidden_size, config.hidden_size),
+            LayerNorm = nn.LayerNorm(config.hidden_size)
+        ))
+        self.decoder = nn.Linear(config.hidden_size, config.vocab_size)
+
+        self.decoder.bias = self.bias
+
+    def forward(self, x):
+        x = self.transform.dense(x)
+        x = F.gelu(x)
+        x = self.transform.LayerNorm(x)
+        logits = self.decoder(x)
+        return logits
+
+class skBERTForMaskedLM(nn.Module):
+    def __init__(self, config: skBERTConfig):
+        super().__init__()
         self.bert = skBERT(config)
         self.cls = nn.ModuleDict(dict(
-            predictions = nn.ModuleDict(dict(
-                bias = nn.Parameter(torch.zeros(config.n_embd)),
-                transform = nn.ModuleDict(dict(
-                    dense = nn.Linear(config.n_embd, config.n_embd),
-                    LayerNorm = nn.LayerNorm(config.n_embd)
-                ))
-            )),
-            decoder = nn.Linear(config.n_embd, config.vocab_size)
+            predictions = BERTPredictionHeadLM(config),
         ))
-
-        self.cls.decoder.bias = self.cls.predictions.bias
-        self.block_size = config.block_size
+        self.block_size = config.max_position_embeddings
     
+    @classmethod
+    def from_pretrained(cls, model_name_or_path: str):
+        config = BertConfig.from_pretrained(model_name_or_path)
+        model = cls(config)
+
+        state_dict = AutoModelForMaskedLM.from_pretrained(model_name_or_path).state_dict()
+        model.load_state_dict(state_dict)
+
+        return model
     def forward(self, x, attn_mask=None, token_type_ids=None, targets=None):
-        B, T = x.shape
-
         x = self.bert(x, attn_mask=attn_mask, token_type_ids=token_type_ids) # (B, T) -> (B, T, C)
-        x = self.cls.transform.dense(x)
-        x = F.gelu(x)
-        x = self.cls.transform.LayerNorm(x)
-
-        logits = self.decoder(x)
+        logits = self.cls.predictions(x)
         loss = None
 
         if targets is not None:
@@ -145,6 +162,7 @@ class skBERTForMaskedLM(nn.Module):
 
     def generate(self, input_tensor: Tensor, attn_mask: Tensor, token_type_ids: Tensor, max_new_tokens: int):
         for _ in range(max_new_tokens):
+            input_tensor = 
             input_cropped = input_tensor[:, -self.block_size:]
 
             logits, loss = self(input_cropped)
@@ -171,8 +189,9 @@ def mask_inputs_for_mlm(inputs: Tensor, tokenizer: BertTokenizer, mlm_probabilit
     return inputs, labels
 
 
-model = skBERTForMaskedLM(skBERTConfig)
-
+model = skBERTForMaskedLM.from_pretrained("google-bert/bert-base-uncased")
+# model = skBERTForMaskedLM(skBERTConfig)
+print('Woo!')
 tokenizer: BertTokenizer = BertTokenizer.from_pretrained("google-bert/bert-base-uncased")
 
 input_docs = [
@@ -182,8 +201,17 @@ input_docs = [
 ]
 tokenized_inputs = tokenizer(input_docs)
 
-input_toks = tokenized_inputs['input_ids']
-attn_masks = tokenized_inputs['attention_mask']
-token_type_ids = tokenized_inputs['token_type_ids']
+input_toks = torch.tensor(tokenized_inputs['input_ids'])
+attn_masks = torch.tensor(tokenized_inputs['attention_mask'])
+token_type_ids = torch.tensor(tokenized_inputs['token_type_ids'])
 
-x = model.generate(input_tensor=input_toks, attn_mask=attn_masks, token_type_ids=token_type_ids)
+x = model.generate(input_tensor=input_toks, attn_mask=attn_masks, token_type_ids=token_type_ids, max_new_tokens=50)
+
+for gen in x:
+    print(tokenizer.decode(gen))
+
+# ------------------------------------ training loop ------------------------------------
+
+class DataloaderLite():
+    def __init__(self) -> None:
+        pass
